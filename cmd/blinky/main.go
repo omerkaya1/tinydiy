a3f34555e7d6719d39b1530a91e846505d3c3dfc
Omer Kaya <omerkaya513@gmail.com>
2025-04-04 21:08:43 +0300

    [add]: external led

--- cmd/blinky/main.go
+++ cmd/blinky/main.go
@@ -10,13 +10,28 @@ func main() {
 	led := machine.LED
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
+	cfg := machine.PinConfig{Mode: machine.PinOutput}
+
+	externalLed := machine.D13
+	externalLed.Configure(cfg)
+
 	for {
 		led.High()
+		externalLed.High()
+
 		time.Sleep(time.Millisecond * 1000)
+
+		externalLed.Low()
 		led.Low()
-		time.Sleep(time.Millisecond * 50)
+
+		time.Sleep(time.Millisecond * 200)
+
 		led.High()
-		time.Sleep(time.Millisecond * 50)
+		externalLed.High()
+
+		time.Sleep(time.Millisecond * 500)
+
 		led.Low()
+		externalLed.Low()
 	}
 }
